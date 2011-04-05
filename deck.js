1a0ed17d6741b9661a7b925e20bfe1370b8aadf8
Joe Bauser <coderjoe@coderjoe.net>
2011-04-04 21:52:36 -0400

    Adding fisher yates and removing new this context on shuffle method as it is unnecessary

--- deck.js
+++ deck.js
@@ -3,6 +3,10 @@
  * and a function pointer to a shuffle method which accepts an array of cards
  */
 function Deck( card_types, shuffle_method ) {
+    if( shuffle_method == undefined ) {
+        throw 'Undefined Shuffle Method';
+    }
+
     this.cards = card_types;
     this.deck = card_types;
     this.shuffleMethod = shuffle_method;
@@ -16,7 +20,9 @@ Deck.prototype.setShuffleMethod = function( shuffle_method ) {
 }
 
 /*
- * Deal a number of cards off of the deck
+ * Deal an array of cards cards off of the deck
+ * If the deck is empty the array will be empty
+ *
  */
 Deck.prototype.deal = function( num_cards ) {
     return this.cards.splice(0,num_cards);
@@ -26,8 +32,7 @@ Deck.prototype.deal = function( num_cards ) {
  * Shuffle this deck
  */
 Deck.prototype.shuffle = function() {
-    //Call our functor with the "this" context of our Deck object.
-    this.deck =  this.shuffleMethod.call( this );
+    this.deck =  this.shuffleMethod( this.deck );
 };
 
 /*
@@ -38,21 +43,40 @@ Deck.prototype.reset = function() {
 };
 
 /*
+ * Display the deck as a string of comma separated values
+ */
+Deck.prototype.toString = function() {
+    return this.deck.join(', ');
+}
+
+/*
  * Just a namespace to store our shuffle methods
  * No real reason other than for fun
  *
  * Each shuffle method can assume it has access to
- * this.deck and this.
+ * this. Not really necessary since we pass the deck... but could be useful later;
  */
 var ShuffleMethod = {
     /*
      * A simple Math.random shuffle.
      * Semi-bias towards the original position thanks to the way the sort method works
      */
-    randomShuffle: function() {
-        console.log('Shuffling...');
-        return this.deck.sort( function() { 
+    randomShuffle: function( deck ) {
+        return deck.sort( function() { 
                 return Math.floor(Math.random()*3+1)-2;
         } );
+    },
+
+    modernFisherYates: function( deck ) {
+        var decksize = deck.length;
+        for( var i = (decksize-1); i >= 1; i-- ) {
+            //A random number between 0 and length-1
+            var j = Math.floor(Math.random()*(i+1));
+            var temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+
+        return deck;
     }
 };
